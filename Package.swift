9c92fe54af4a8a1a422dd71fa38c3ea2e045c625
Camden Fullmer <camdenfullmer@gmail.com>
2025-07-30 17:17:57 -0400

    Test defining directly.

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 import Foundation
 
 var additionalCsettings: [CSetting] = []
-if ProcessInfo.processInfo.environment["SQLITE_ENABLE_FTS5"] == "1" {
-    additionalCsettings.append(.define("SQLITE_ENABLE_FTS5"))
-}
+//if ProcessInfo.processInfo.environment["SQLITE_ENABLE_FTS5"] == "1" {
+//    additionalCsettings.append(.define("SQLITE_ENABLE_FTS5"))
+//}
 
 let package = Package(
   name: "swift-toolchain-sqlite",
@@ -26,6 +26,7 @@ let package = Package(
         .define(
           "SQLITE_NOHAVE_SYSTEM",
           .when(platforms: [.macCatalyst, .iOS, .tvOS, .watchOS, .visionOS, .wasi])),
+        .define("SQLITE_ENABLE_FTS5"),
         .define("HAVE_READLINE", .when(platforms: [.macOS, .macCatalyst])),
         .define("_WASI_EMULATED_SIGNAL", .when(platforms: [.wasi])),
         .define("_WASI_EMULATED_PROCESS_CLOCKS", .when(platforms: [.wasi])),
